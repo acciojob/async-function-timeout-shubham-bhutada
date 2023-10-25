781c5ca84d57580885e36048b6c39f4d9a78b8bc
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-10-25 13:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const submitBtn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+async function delayOperation(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function showData() {
+  await delayOperation(delay.value);
+  output.innerHTML = "";
+  output.innerText = text.value;
+}
+
+
+submitBtn.addEventListener("click", showData);
